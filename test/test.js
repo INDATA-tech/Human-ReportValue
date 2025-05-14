94b2a8a6bb662a3556f0cc34b61d50c231280dbb
sahin.erdogan <sahin.erdogan@indata.com.tr>
2025-05-14 16:40:06 +0300

    updated İş Yaşamında Rolün texts

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test_main from "../src/test_main.js";
 import main from "../src/main.js";
 
-var req = `67c593868ee05c1fa029`;
+var req = `682392e2805a5ba538ce`;
 
 var res = "";
 var log = "";
