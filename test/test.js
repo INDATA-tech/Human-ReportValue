a233f5dddd801196bc877ae2691e1bba01f6a44e
sahin.erdogan <sahin.erdogan@indata.com.tr>
2025-05-21 13:58:04 +0300

    updated kişilik özellikleri and karakter özellikleri

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 import test_main from "../src/test_main.js";
 import main from "../src/main.js";
 
-var req = `68278e0f9f8c64229e33`;
+var req = `67c593868ee05c1fa029`;
 
 var res = "";
 var log = "";
